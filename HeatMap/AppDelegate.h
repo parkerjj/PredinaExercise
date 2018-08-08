8de858e1afe71b922a2b5a7ccba222c5a309e59c
Parker <Parker@liuyuedemacbook-air.local>
2018-08-08 16:04:34 +0800

    Initial Commit

---
+++ HeatMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeatMap
+//
+//  Created by Parker on 2018/8/8.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeatMap/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HeatMap
+//
+//  Created by Parker on 2018/8/8.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
